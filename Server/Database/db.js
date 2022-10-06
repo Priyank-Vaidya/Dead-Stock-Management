cf31d61c4d3a5e5ed89f00fc5f4f73a12b12cf11
Priyank Vaidya <priyankvaidya09@gmail.com>
2022-10-06 10:22:16 +0530

    db.js MOdified

--- Server/Database/db.js
+++ Server/Database/db.js
@@ -4,8 +4,8 @@ const pool = new Pool({
     user : "postgres",
     password : "Priyank",
     host : "localhost",
-    port : "5432",
+    port : 5432,
     database : "pertendo"
 });
 
-module.exports = Pool;
+module.exports = pool;
